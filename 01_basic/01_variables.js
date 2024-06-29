96a39d04e559a5555e751f150cf250dab70b08e0
dharmik-anghan <dharmikanghan09@gmail.com>
2024-06-29 14:49:00 +0530

    basic

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "dharmikanghan09@gmail.com"
+var accountPassword = "dharmik"
+accountCity = "Surat"
+let accountState;
+// accountId = 2
+
+accountEmail = "ChangedEmail"
+accountPassword = "ChangePassword"
+accountCity = "ChangeCity"
+
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) // We are using NodeJS, not browser
+
+console.log(3 + 3); // Code redability should be high
+
+console.log("Dharmik\nAnghan");
+
+let name = "dharmik"
+let age = 21
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => false/true
+// null => standalone value 
+// undefined => 
+// symbol => mostly used in react to find uniqueness
+
+
+// object
+
+console.log(typeof undefined); // type -> undefined
+console.log(typeof null); // tpye -> object
